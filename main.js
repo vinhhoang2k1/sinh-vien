1d4b9d280d14407249e923725d242dc1f21b6e0d
HOÀNG ĐỨC VINH <20200120@gmail.com>
2021-10-26 16:08:10 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var idSVList = document.querySelectorAll('tr td#msv');
+idSVList.forEach((e) => {
+
+
+})
+var html = idSVList.forEach((idSV) => {
+    let i = 0;
+    idSV.innerText = `2020 ${i++}`;
+    console.log(i)
+    for (let i = 0; i < idSVList.length; i++) {
+
+    }
+    // return ` <td id="msv">2020 ${i++}</td> 
+    //     `
+})
